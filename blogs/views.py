4e8e723c80d6da355ac560a0392e6413d05d2b22
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-04-16 01:27:45 +0530

    Additions: - Template directory - Signup and Signin page - gitignore file - urls.py
    Updation:
    - Settings.py

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello")
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
